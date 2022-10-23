24d39804ce22fed7d07c2ea2128b53e717ceca57
treelun <109446729+treelun@users.noreply.github.com>
2022-10-23 10:59:58 +0900

    10-23 class

---
+++ 10-23 class/Assets/Script/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRigidbody;
+    public float speed = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
