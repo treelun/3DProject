14af6c35f04b77c000529ca681f123d57f25949a
treelun <109446729+treelun@users.noreply.github.com>
2022-11-02 14:34:09 +0900

    11-02

--- 10-30 class/Assets/Scripts/AmmoPack.cs
+++ 10-30 class/Assets/Scripts/AmmoPack.cs
@@ -1,55 +1,55 @@
 ﻿#region 15
-//using UnityEngine;
-
-//총알을 충전하는 아이템
-//public class AmmoPack : MonoBehaviour, IItem
-//{
-//    public int ammo = 30; // 충전할 총알 수
-
-//    public void Use(GameObject target)
-//    {
-//        전달 받은 게임 오브젝트로부터 PlayerShooter 컴포넌트를 가져오기 시도
-//        PlayerShooter playerShooter = target.GetComponent<PlayerShooter>();
-
-//        PlayerShooter 컴포넌트가 있으며, 총 오브젝트가 존재하면
-//        if (playerShooter != null && playerShooter.gun != null)
-//        {
-//            총의 남은 탄환 수를 ammo 만큼 더한다
-//            playerShooter.gun.ammoRemain += ammo;
-//        }
-
-//        사용되었으므로, 자신을 파괴
-//        Destroy(gameObject);
-//    }
-//}
-#endregion
-
-
-#region 19
-
-using Photon.Pun;
 using UnityEngine;
 
-// 총알을 충전하는 아이템
-public class AmmoPack : MonoBehaviourPun, IItem
+//총알을 충전하는 아이템
+public class AmmoPack : MonoBehaviour, IItem
 {
     public int ammo = 30; // 충전할 총알 수
 
     public void Use(GameObject target)
     {
-        // 전달 받은 게임 오브젝트로부터 PlayerShooter 컴포넌트를 가져오기 시도
+        //전달 받은 게임 오브젝트로부터 PlayerShooter 컴포넌트를 가져오기 시도
         PlayerShooter playerShooter = target.GetComponent<PlayerShooter>();
 
-        // PlayerShooter 컴포넌트가 있으며, 총 오브젝트가 존재하면
+       // PlayerShooter 컴포넌트가 있으며, 총 오브젝트가 존재하면
         if (playerShooter != null && playerShooter.gun != null)
         {
-            // 총의 남은 탄환 수를 ammo 만큼 더하기, 모든 클라이언트에서 실행
-            playerShooter.gun.photonView.RPC("AddAmmo", RpcTarget.All, ammo);
+            //총의 남은 탄환 수를 ammo 만큼 더한다
+            playerShooter.gun.ammoRemain += ammo;
         }
 
-        // 모든 클라이언트에서의 자신을 파괴
-        PhotonNetwork.Destroy(gameObject);
+        //사용되었으므로, 자신을 파괴
+        Destroy(gameObject);
     }
 }
-
 #endregion
+
+
+//#region 19
+
+//using Photon.Pun;
+//using UnityEngine;
+
+//// 총알을 충전하는 아이템
+//public class AmmoPack : MonoBehaviourPun, IItem
+//{
+//    public int ammo = 30; // 충전할 총알 수
+
+//    public void Use(GameObject target)
+//    {
+//        // 전달 받은 게임 오브젝트로부터 PlayerShooter 컴포넌트를 가져오기 시도
+//        PlayerShooter playerShooter = target.GetComponent<PlayerShooter>();
+
+//        // PlayerShooter 컴포넌트가 있으며, 총 오브젝트가 존재하면
+//        if (playerShooter != null && playerShooter.gun != null)
+//        {
+//            // 총의 남은 탄환 수를 ammo 만큼 더하기, 모든 클라이언트에서 실행
+//            playerShooter.gun.photonView.RPC("AddAmmo", RpcTarget.All, ammo);
+//        }
+
+//        // 모든 클라이언트에서의 자신을 파괴
+//        PhotonNetwork.Destroy(gameObject);
+//    }
+//}
+
+//#endregion
--- 10-30 class/Assets/Scripts/PlayerMovement.cs
+++ 10-30 class/Assets/Scripts/PlayerMovement.cs
@@ -1,72 +1,9 @@
-﻿#region 19
-using Photon.Pun;
-using UnityEngine;
-
-// 플레이어 캐릭터를 사용자 입력에 따라 움직이는 스크립트
-public class PlayerMovement : MonoBehaviourPun
-{
-    public float moveSpeed = 5f; // 앞뒤 움직임의 속도
-    public float rotateSpeed = 180f; // 좌우 회전 속도
-
-    private Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
-    private PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트
-    private Rigidbody playerRigidbody; // 플레이어 캐릭터의 리지드바디
-
-    private void Start()
-    {
-        // 사용할 컴포넌트들의 참조를 가져오기
-        playerInput = GetComponent<PlayerInput>();
-        playerRigidbody = GetComponent<Rigidbody>();
-        playerAnimator = GetComponent<Animator>();
-    }
-
-    // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
-    private void FixedUpdate()
-    {
-        // 로컬 플레이어만 직접 위치와 회전을 변경 가능
-        if (!photonView.IsMine)
-        {
-            return;
-        }
-
-        // 회전 실행
-        Rotate();
-        // 움직임 실행
-        Move();
-
-        // 입력값에 따라 애니메이터의 Move 파라미터 값을 변경
-        playerAnimator.SetFloat("Move", playerInput.move);
-    }
-
-    // 입력값에 따라 캐릭터를 앞뒤로 움직임
-    private void Move()
-    {
-        // 상대적으로 이동할 거리 계산
-        Vector3 moveDistance =
-            playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
-        // 리지드바디를 통해 게임 오브젝트 위치 변경
-        playerRigidbody.MovePosition(playerRigidbody.position + moveDistance);
-    }
-
-    // 입력값에 따라 캐릭터를 좌우로 회전
-    private void Rotate()
-    {
-        // 상대적으로 회전할 수치 계산
-        float turn = playerInput.rotate * rotateSpeed * Time.deltaTime;
-        // 리지드바디를 통해 게임 오브젝트 회전 변경
-        playerRigidbody.rotation =
-            playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
-    }
-}
-#endregion
-
-
-#region 14
-
+﻿//#region 19
+//using Photon.Pun;
 //using UnityEngine;
 
 //// 플레이어 캐릭터를 사용자 입력에 따라 움직이는 스크립트
-//public class PlayerMovement : MonoBehaviour
+//public class PlayerMovement : MonoBehaviourPun
 //{
 //    public float moveSpeed = 5f; // 앞뒤 움직임의 속도
 //    public float rotateSpeed = 180f; // 좌우 회전 속도
@@ -74,7 +11,6 @@ public class PlayerMovement : MonoBehaviourPun
 //    private Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
 //    private PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트
 //    private Rigidbody playerRigidbody; // 플레이어 캐릭터의 리지드바디
-    
 
 //    private void Start()
 //    {
@@ -82,12 +18,17 @@ public class PlayerMovement : MonoBehaviourPun
 //        playerInput = GetComponent<PlayerInput>();
 //        playerRigidbody = GetComponent<Rigidbody>();
 //        playerAnimator = GetComponent<Animator>();
-        
 //    }
 
 //    // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
 //    private void FixedUpdate()
 //    {
+//        // 로컬 플레이어만 직접 위치와 회전을 변경 가능
+//        if (!photonView.IsMine)
+//        {
+//            return;
+//        }
+
 //        // 회전 실행
 //        Rotate();
 //        // 움직임 실행
@@ -103,8 +44,6 @@ public class PlayerMovement : MonoBehaviourPun
 //        // 상대적으로 이동할 거리 계산
 //        Vector3 moveDistance =
 //            playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
-//        //characterController.Move(moveDistance);
-
 //        // 리지드바디를 통해 게임 오브젝트 위치 변경
 //        playerRigidbody.MovePosition(playerRigidbody.position + moveDistance);
 //    }
@@ -113,11 +52,104 @@ public class PlayerMovement : MonoBehaviourPun
 //    private void Rotate()
 //    {
 //        // 상대적으로 회전할 수치 계산
-//        float turn =
-//            playerInput.rotate * rotateSpeed * Time.deltaTime;
+//        float turn = playerInput.rotate * rotateSpeed * Time.deltaTime;
 //        // 리지드바디를 통해 게임 오브젝트 회전 변경
-//        playerRigidbody.rotation = playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
+//        playerRigidbody.rotation =
+//            playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
 //    }
 //}
+//#endregion
+
+
+#region 14
+
+using UnityEngine;
+
+// 플레이어 캐릭터를 사용자 입력에 따라 움직이는 스크립트
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // 앞뒤 움직임의 속도
+    public float rotateSpeed = 180f; // 좌우 회전 속도
+
+    private Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
+    private PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트
+
+    CharacterController character;
+    Vector3 movement;
+    float gravity = -9.8f;
+    private void Start()
+    {
+        // 사용할 컴포넌트들의 참조를 가져오기
+        playerInput = GetComponent<PlayerInput>();
+        playerAnimator = GetComponent<Animator>();
+        character = GetComponent<CharacterController>();
+
+    }
+
+    // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
+    private void FixedUpdate()
+    {
+        // 회전 실행
+        Rotate();
+        // 움직임 실행
+
+        Move();
+        // 입력값에 따라 애니메이터의 Move 파라미터 값을 변경
+        if (playerInput.move != 0)
+        {
+            playerAnimator.SetFloat("Move", playerInput.move);
+
+        }
+        else if (playerInput.rotate != 0)
+        {
+            playerAnimator.SetFloat("Move", playerInput.rotate);
+
+        }
+
+    }
+
+    // 입력값에 따라 캐릭터를 앞뒤로 움직임
+    private void Move()
+    {
+
+        float deltaX = playerInput.rotate;
+        float deltaZ = playerInput.move;
+
+        movement = new Vector3(deltaX, 0, deltaZ) * Time.deltaTime;
+        movement.y = gravity;
+
+        character.Move(movement * moveSpeed);
+
+
+
+
+    }
+
+    // 입력값에 따라 캐릭터를 좌우로 회전
+    private void Rotate()
+    {
+        // 상대적으로 회전할 수치 계산
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray , out hit))
+        {
+            //레이가 맞은곳에서 캐릭터의 위치를빼면 캐릭터에서 마우스로 향하는 방향을 알수있음
+            //단 레이가 맞은곳의 y좌표는 캐릭터의 y좌표로함 안그러면 캐릭터의 위아래방향도 인식되어 캐릭터가 기울어질수있음
+            Vector3 mouseDir = new Vector3(hit.point.x, transform.position.y, hit.point.z) - transform.position;
+            transform.forward = mouseDir;
+            
+        }
+
+        
+        //transform.localEulerAngles = mousePos; 
+
+        // 리지드바디를 통해 게임 오브젝트 회전 변경
+        //playerRigidbody.rotation = playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
+
+        //playerRotate = Input.GetAxis("Mouse X");
+        //transform.Rotate(0, playerRotate * rotateSpeed * Time.deltaTime, 0, Space.World);
+    }
+}
 
 #endregion
